da6cdfc4cfeb2b448359317ee45ef7bf9bcf62bb
Kavin-0307 <sharmakavin03@gmail.com>
2026-05-02 14:11:18 +0530

    Session module under progress

--- backend/src/main/java/com/bitfocus/backend/session/SessionRepository.java
+++ backend/src/main/java/com/bitfocus/backend/session/SessionRepository.java
@@ -1,5 +1,10 @@
 package com.bitfocus.backend.session;
 
-public class SessionRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface SessionRepository extends JpaRepository<Session, Long> {
+
+    List<Session> findByTask_TaskId(Long taskId);
 
 }
--- backend/src/main/java/com/bitfocus/backend/session/SessionService.java
+++ backend/src/main/java/com/bitfocus/backend/session/SessionService.java
@@ -1,6 +1,12 @@
 package com.bitfocus.backend.session;
 
+import java.time.LocalDateTime;
+
 import com.bitfocus.backend.game.GameService;
+import com.bitfocus.backend.session.dtos.SessionEndRequestDTO;
+import com.bitfocus.backend.session.dtos.SessionStartRequestDTO;
+import com.bitfocus.backend.task.Task;
+import com.bitfocus.backend.task.TaskCompletion;
 import com.bitfocus.backend.task.TaskRepository;
 import com.bitfocus.backend.task.TaskService;
 public class SessionService {
@@ -15,8 +21,18 @@ public class SessionService {
 		this.taskService=taskService;
 	}
 	public Session startSession(SessionStartRequestDTO request) {
-		Task task=taskRepository.findById(getTaskId()).orElseThrow(()->new IllegalArgumentException("Task not found"));
+		Task task=taskRepository.findById(request.getTaskId()).orElseThrow(()->new IllegalArgumentException("Task not found"));
+		if(task.getTaskStatus()==TaskCompletion.COMPLETED||task.getTaskStatus()==TaskCompletion.ABANDONED) {
+				throw new IllegalArgumentException("Cannot start session for inactive task");
+		}
+		Session session=new Session();
+		session.setTask(task);
+		session.setStartTime(LocalDateTime.now());
+		return sessionRepository.save(session);
 	}
 
-	public TaskResponseDTO endSession(SessionEndRequestDTO request);
+
+	public TaskResponseDTO endSession(SessionEndRequestDTO request) {
+		Session session=sessionRepository.findById(request.getSessionId()).orElseThrow(()->new IllegalArgumentException("Session not found"));
+	}
 }
