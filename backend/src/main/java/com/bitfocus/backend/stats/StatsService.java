9fd43af55ee31dcbbf34c11ff6f7a61f72c30c8c
Kavin-0307 <sharmakavin03@gmail.com>
2026-05-02 15:13:43 +0530

    The last part-statistics

--- backend/src/main/java/com/bitfocus/backend/stats/StatsService.java
+++ backend/src/main/java/com/bitfocus/backend/stats/StatsService.java
@@ -13,5 +13,29 @@ import com.bitfocus.backend.task.TaskRepository;
 
 @Service
 public class StatsService {
-
+	private final SessionRepository sessionRepository;
+	private final TaskRepository taskRepository;
+	public StatsService(TaskRepository taskRepository,SessionRepository sessionRepository) {
+		this.sessionRepository=sessionRepository;
+		this.taskRepository=taskRepository;
+	}
+	public StatsResponseDTO getStats() {
+		List<Session> sessions=sessionRepository.findAll();
+		List<Task> tasks=taskRepository.findAll();
+		
+		
+		int totalFocusTime=sessions.stream().filter(s->s.getEndTime()!=null).mapToInt(Session::getDurationSeconds).sum();
+		int totalSessions = (int) sessions.stream().filter(s->s.getEndTime()!=null).count();
+		
+		
+		
+		long completedTasks=tasks.stream().filter(t->t.getTaskStatus()==TaskCompletion.COMPLETED).count();
+		long totalTasks=tasks.size();
+		
+        double completionRate=totalTasks==0?0:((double)completedTasks/ totalTasks)*100;
+		int productivityScore=(totalSessions*10)+(int)(completionRate*50);
+		return new StatsResponseDTO(totalSessions,totalFocusTime,completionRate,productivityScore);
+		
+		
+	}
 }
